54a126e4dfa1a9fb0620af8cf53ee6977af0d1ce
chuff-chuff <norizaki85@gmail.com>
2019-01-03 15:18:13 +0000

    fourth commit

---
+++ showlink.js
@@ -0,0 +1,17 @@
+// モジュール野読み込み
+var client = require('cheerio-httpcli');
+
+// ダウンロード
+var url = "http://www.aozora.gr.jp/index_pages/person81.html";
+var param = {};
+client.fetch(url, param, function (err, $, res) {
+      if (err) {
+        console.log("error");
+        return;
+      }
+      $("a").each(function (idx) {
+          var text = $(this).text();
+          var href = $(this).attr("href");
+          console.log(text + ":" + href);
+          });
+      });
